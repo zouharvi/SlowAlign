5c6e819ddc2418dd8fc1ab9ff373502444e1c4c0
Vilém Zouhar <vilem.zouhar@gmail.com>
2021-01-10 13:06:09 +0100

    E normalization multithreading, faster than fast_align

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ use std::io::BufReader;
 use std::path::Path;
 
 fn main() {
+    const SENT_COUNT: usize = 100000;
+    const E_THREADS: usize = 8;
+    const E_CHUNKS: usize = SENT_COUNT / E_THREADS;
+    const M_THREADS: usize = 8;
     // load data
     let file1 = File::open(Path::new("data/hansards.e")).unwrap();
     let file2 = File::open(Path::new("data/hansards.f")).unwrap();
     let reader1 = BufReader::new(file1);
     let reader2 = BufReader::new(file2);
-    let mut sents = Vec::<(Vec<usize>, Vec<usize>)>::new();
+    let mut sents = Vec::<(Vec<usize>, Vec<usize>)>::with_capacity(SENT_COUNT);
     let mut vocab1 = HashMap::<String, usize>::new();
     let mut vocab2 = HashMap::<String, usize>::new();
-
-    for (line1, line2) in reader1.lines().zip(reader2.lines()).take(100000) {
+    for (line1, line2) in reader1.lines().zip(reader2.lines()).take(SENT_COUNT) {
         let line1 = line1.unwrap();
         let line2 = line2.unwrap();
         let tokens1 = line1
@@ -39,44 +42,52 @@ fn main() {
     }
     let mut alignment_probs = sents
         .iter()
-        .map(|(s1, s2)| vec![vec![1.0; s1.len()]; s2.len()])
+        .map(|(s1, s2)| vec![vec![1.0 / (s1.len() as f32); s1.len()]; s2.len()])
         .collect::<Vec<Vec<Vec<f32>>>>();
-
     let v1_len = vocab1.keys().len();
     let v2_len = vocab2.keys().len();
-
-    const CHUNK_SIZE: usize = 100000/8;
-
+    let m_chunks: usize = v1_len / M_THREADS;
     // EM loop
     for step in 0..5 {
         eprintln!("step {}", step);
-
         // expectation
         let mut word_probs = vec![vec![0.0; v1_len]; v2_len];
         for ((s1, s2), probs) in sents.iter().zip(alignment_probs.iter()) {
             for (word_tgt, probs_tgt) in s2.iter().zip(probs.iter()) {
                 for (word_src, partial_count) in s1.iter().zip(probs_tgt) {
-                    word_probs[*word_tgt][*word_src] += *partial_count as f32;
+                    word_probs[*word_tgt][*word_src] += *partial_count;
                 }
             }
         }
-        // normalize rows
-        for word_prob in word_probs.iter_mut() {
-            let sum = word_prob.iter().sum::<f32>();
-            for prob in word_prob.iter_mut() {
-                *prob /= sum;
+
+        // thread at least normalization
+        let _ = crossbeam::scope(|scope| {
+            for word_probs in word_probs.chunks_mut(m_chunks) {
+                scope.spawn(move |_| {
+                    // normalize rows
+                    for word_prob in word_probs.iter_mut() {
+                        let sum = word_prob.iter().sum::<f32>();
+                        for prob in word_prob.iter_mut() {
+                            *prob /= sum;
+                        }
+                    }
+                });
             }
-        }
+        });
 
         let _ = crossbeam::scope(|scope| {
             // chop alignment_probs into disjoint sub-slices
-            for (chunk_i, alignment_probs_slice) in alignment_probs.chunks_mut(CHUNK_SIZE).enumerate() {
+            for (chunk_i, alignment_probs_slice) in alignment_probs.chunks_mut(E_CHUNKS).enumerate()
+            {
                 // shadow variables so that they are taken as immutable references
                 let sents = &sents;
                 let word_probs = &word_probs;
                 scope.spawn(move |_| {
                     // maximization
-                    for (sent_i, (s1, s2)) in sents[chunk_i*CHUNK_SIZE..(chunk_i+1)*CHUNK_SIZE].iter().enumerate() {
+                    for (sent_i, (s1, s2)) in sents[chunk_i * E_CHUNKS..(chunk_i + 1) * E_CHUNKS]
+                        .iter()
+                        .enumerate()
+                    {
                         let sent_probs = &mut alignment_probs_slice[sent_i];
                         for (pos1, word1) in s1.iter().enumerate() {
                             for (pos2, word2) in s2.iter().enumerate() {
